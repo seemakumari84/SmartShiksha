48c2a5006ad05a9babd7fe21218f51ab43dbe3a3
Rani kumari <162621073+Ranikumari16@users.noreply.github.com>
2025-04-13 15:48:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCr0UhA_7M2GdjeP-D2xxqERClmnISP-n8",
+  authDomain: "smartshiksha-nitp081.firebaseapp.com",
+  projectId: "smartshiksha-nitp081",
+  storageBucket: "smartshiksha-nitp081.firebasestorage.app",
+  messagingSenderId: "400008026803",
+  appId: "1:400008026803:web:b734585b70cf8efc76eec6",
+  measurementId: "G-5FDR9DY7ZZ"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db, app };
